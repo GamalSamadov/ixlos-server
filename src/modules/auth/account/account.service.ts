e31770464ce215fdfa11dbeead828dbbb4a3895c
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-01-30 16:48:40 +0300

    Account initial implement

---
+++ src/modules/auth/account/account.service.ts
@@ -0,0 +1,50 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+
+import { PrismaService } from '@/src/core/prisma/prisma.service'
+
+import { CreateUserInput } from './input/create-user.input'
+
+@Injectable()
+export class AccountService {
+	public constructor(private readonly prismaService: PrismaService) {}
+
+	public async findAll() {
+		return this.prismaService.user.findMany()
+	}
+
+	public async createUser(input: CreateUserInput) {
+		const { email, username, password } = input
+
+		const isUsernameExist = await this.prismaService.user.findUnique({
+			where: {
+				username
+			}
+		})
+
+		if (isUsernameExist) {
+			throw new ConflictException('Username already exist')
+		}
+
+		const isEmailExist = await this.prismaService.user.findUnique({
+			where: {
+				email
+			}
+		})
+
+		if (isEmailExist) {
+			throw new ConflictException('Email already exist')
+		}
+
+		await this.prismaService.user.create({
+			data: {
+				username,
+				email,
+				password: await hash(password),
+				displayName: username
+			}
+		})
+
+		return true
+	}
+}
